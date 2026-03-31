e013ae57684c9551a951eaebae7fad48b62019c6
Samuli Laiho <samuli.laiho@helsinki.fi>
2026-03-31 21:34:39 +0300

    update invoke-fuctions

--- tasks.py
+++ tasks.py
@@ -11,8 +11,8 @@ def test(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("poetry run coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task
 def coverage_report(ctx):
-    ctx.run("poetry run coverage report -m", pty=True)
+    ctx.run("coverage html", pty=True)
